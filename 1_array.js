fddf7e695fce8b27a4ad15b70b4e4b71c199a463
Apon Roy <roy.apon.700@gmail.com>
2025-06-01 09:59:30 +0000

    Practiced some methods of array.

---
+++ 1_array.js
@@ -0,0 +1,55 @@
+// const myHeros = new Array("Spidermen", "Supermen", "Batmen")
+// console.log(myHeros);
+
+// const myDesk = new Array;
+// myDesk.push("Books")
+// myDesk.push("Pens")
+// myDesk.push("Notes")
+// myDesk.push("Water_Pot")
+// myDesk.pop()
+
+// console.log(myDesk);
+
+// const marks = [1, 2, 3, 4, 5, 6]
+// marks.shift()
+// console.log(marks);
+
+// let bools = marks.includes(9)
+// console.log(bools);
+
+// let has = marks.includes(2)
+// console.log(has);
+
+// Splice and Slice
+
+// const marks = [1, 2, 3, 4, 5, 6]
+
+// let slicing_part = marks.slice(1,4)
+// console.log(slicing_part);
+
+// let slicing_part = marks.slice(-3,-1)
+// console.log(slicing_part);
+
+// let markss = [1, 2, 3, 4, 5, 6]
+
+
+// let splicing_part = markss.splice( 0,4)
+// console.log(markss)
+// console.log(splicing_part);
+
+// Slice => It acutally takes a copy of the original array and slice the array and provides new array
+// On the other hand, splice manipulates the original array. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
