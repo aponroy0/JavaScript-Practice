1df848b8bc9da301f730926c0323bc33cc3749ed
Apon Roy <roy.apon.700@gmail.com>
2025-06-01 10:49:46 +0000

    Concat, spread, array from, array of

---
+++ 2_array.js
@@ -0,0 +1,38 @@
+// const marvel_heros = ["thor", "Ironman", "spiderman"]
+// const dc_heros = ["superman", "flash", "batman"]
+
+
+
+// //marvel_heros.push(dc_heros)
+// // const allHeros = marvel_heros.concat(dc_heros)
+
+// // Using spread
+// const allHeros = [...marvel_heros, ...dc_heros]
+// console.log(allHeros);
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+
+// console.log(Array.of(score1, score2, score3));
+
+// console.log(Array.isArray("Hitesh"))
+// console.log(Array.from("Hitesh"))
+// console.log(Array.from({name: "hitesh"})) // interesting
+
+console.log(Array.isArray("Apon"));
+console.log(Array.from("Apon Roy"))
+console.log(Array.from({name: "Apon"}))
+
+const score1 =100
+const socre2 = 200
+const score3 =300
+
+console.log(Array.of(score1, socre2, score3))
+
+
